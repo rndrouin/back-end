609371a235b6cd71e6403cdcb1cafff58aaaf713
Rob Drouin <rob.drouin@gmail.com>
2024-04-29 14:29:10 -0400

    queries POST & GET endpoints working

--- routes/queries/queries.js
+++ routes/queries/queries.js
@@ -1,12 +1,26 @@
 import express from "express";
+import fs from "fs";
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send("GET called");    
+  try {
+    const data = fs.readFileSync('queries.json', 'utf8');
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(404).send("File not found");
+  }
 });
 
 router.post('/', async (req, res) => {
-    res.send("POST called");  
+  try {
+    const savedQueryArray = req.body;
+    const jsonString = JSON.stringify(savedQueryArray);
+    fs.writeFileSync('queries.json', jsonString);
+    res.status(200).send("Query array saved");
+  } catch (err) {
+    res.status(500).send("Failed to save query array");
+  }
 });
 
 export default router;
