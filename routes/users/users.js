f504d068334b02fffd06baace1a67824fefee44f
Rob Drouin <rob.drouin@gmail.com>
2024-04-29 14:10:19 -0400

    users GET endpoint working

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,12 +1,27 @@
 import express from "express";
+import { client, connect } from './db.js';
+
 const router = express.Router();
 
+const dbName = 'usersdb';
+const collectionName = 'users';
+
+connect(); // Connect to MongoDB
+
 router.get('/', async (req, res) => {
-    res.send("GET called");    
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const prj = { user: 1, email: 1, _id: 0 };
+    const users = await collection.find({}).project(prj).toArray();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 router.post('/', async (req, res) => {
-    res.send("POST called");  
+  res.send("POST called");
 });
 
 export default router;
